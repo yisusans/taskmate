94af4538f53b3b8e53a47420dd983048e2a8363d
Ena Bek <enabek@gmail.com>
2016-06-24 17:34:16 -0400

    Add welcome index and controller for page root

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,15 @@ Rails.application.routes.draw do
   resources :memberships, only: :destroy
   resources :groups
   resources :tasks
+  resources :welcome, only: :index
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'groups#index'
+  root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
