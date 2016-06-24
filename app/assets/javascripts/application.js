d733b44885ba75b30ebdb1f69f2283295fd059f7
notjoeyoo <joseph_yoo@brown.edu>
2016-06-24 18:20:47 -0400

    fix facebook confirmation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require materialize-sprockets
 //= require materialize/extras/nouislider
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< 5681325a399321d17f8483a05dd7cef43515f429
-=======
   include ApplicationHelper
->>>>>>> rebase
 
   before_action :authenticate_user!
 
@@ -10,5 +7,5 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  
+
 end
