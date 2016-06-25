342a945918dc4c445f4e42467a9c0875eb0057bc
Ena Bek <enabek@gmail.com>
2016-06-25 16:53:07 -0400

    Add user#show and controller progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by(id: params[:id])
+    @groups = @user.groups
+    @tasks = @user.tasks.order('created_at ASC')
+    @completed_tasks = @user.completed_tasks.order('date_complete DESC')
   end
+
+
 end
