679c5bcee593d6981b599ed49f351a9ca1956873
Susan Yi <yi.susans@gmail.com>
2016-06-25 23:12:31 -0400

    fix group-invite associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ActiveRecord::Base
 	has_many :memberships
 	has_many :users, through: :memberships
 	has_many :tasks
+	has_many :invites
 
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,6 +4,7 @@ class Invite < ActiveRecord::Base
 	belongs_to :invitee, {class_name: "User", foreign_key: :invitee_id}
 	belongs_to :invitor, {class_name: "User", foreign_key: :inviter_id}
 
+	belongs_to :group
 
 
 end
