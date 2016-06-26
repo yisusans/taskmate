bfce7d46b3e993b2ea19055a0991a4c8bf1682c2
Ilya Yampolsky <cfnils@gmail.com>
2016-06-26 11:10:38 -0400

    configure weekly and daily emails to test functionality with heroku scheduler

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -4,4 +4,17 @@ class Mailer < ApplicationMailer
 	  	@user = user
 	  	mail to: "#{user.email}", subject: "Success! test email delivered."
 	end
+
+	def weekly_email(user)
+		@user = user
+		mail to: "#{user.email}", subject: "Hi #{user.name}, here's your weekly taskmate update."
+	end
+
+	def daily_email(user)
+		@user = user
+		mail to: "#{user.email}", subject: "Hey #{user.name}, you have some tasks assigned to you that are due soon."
+	end
+
+
+
 end
