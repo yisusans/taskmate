3497ab4be68ca0305b7e4b061157744bc74259ea
Ena Bek <enabek@gmail.com>
2016-06-26 17:22:03 -0400

    Add chart progress to rebase

---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,44 @@
+$(function () {
+    $(document).ready(function () {
+        $('#container').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Weekly Task Progress'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Tasks',
+                colorByPoint: true,
+                data: [{
+                    name: '',
+                    y: null
+                }]
+            }]
+        });
+    });
+});
+
+
+// Incomplete tasks for week
+// Tasks completed by each member for week
+
+
+
+
