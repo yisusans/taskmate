ceeea47ff0e096582682b8ef13d099fe01a82bfc
notjoeyoo <joseph_yoo@brown.edu>
2016-06-30 15:37:41 -0400

    user scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
 
-  resources :conversations, only: [:create, :show] do
-    resources :messages, only: :create
-  end
+  root 'welcome#index'
+
+  post 'accept' => 'invites#accept'
+  get 'user_groups' => 'groups#user_groups'
+  get 'members' => 'groups#members'
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions',
-        passwords: 'users/passwords',
-        registrations: 'users/registrations',
-        confirmations: 'users/confirmations',
-        omniauth_callbacks: "users/omniauth_callbacks"
-      }
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   resources :users, only: [:index,:show]
   resources :assignments, except: [:edit, :update]
@@ -21,62 +27,8 @@ Rails.application.routes.draw do
   resources :welcome, only: :index
   resources :invites, except: [:index, :edit, :update]
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :conversations, only: [:create, :show] do
+    resources :messages, only: :create
+  end
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  post 'accept' => 'invites#accept'
-  get 'user_groups' => 'groups#user_groups'
-  get 'members' => 'groups#members'
-
-
-  # Example of regular route:
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
