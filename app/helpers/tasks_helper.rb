f4c60aa1bfe237659390484e46225b9595f94172
Susan Yi <yi.susans@gmail.com>
2016-06-30 12:24:56 -0400

    refactor code, change icon for tasks the user has written

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,33 +1,4 @@
 module TasksHelper
 
-	def incomplete_tasks
-		current_user.tasks.select do |task|
-			task.completions.length == 0
-		end
-	end
-
-  	def high_priority
-    	incomplete_tasks.select do |task|
-    	  task.priority == "3"
-    	end
-  	end
-
-  	def med_priority
-    	incomplete_tasks.select do |task|
-    	  task.priority == "2"
-    	end
-  	end
-
-  	def low_priority
-    	incomplete_tasks.select do |task|
-    	  task.priority == "1"
-    	end
-  	end
-
-	def assigned_tasks
-		current_user.assigned_tasks.select do |task|
-			task.completions.length == 0
-		end
-	end
 
 end
