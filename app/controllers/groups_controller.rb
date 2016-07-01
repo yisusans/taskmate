2db20f51660022d848bd6c8ddb68e7d326509baa
Susan Yi <yi.susans@gmail.com>
2016-07-01 07:18:11 -0400

    fix chip icon styling

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,11 +13,8 @@ class GroupsController < ApplicationController
     @task = Task.new
     @membership = Membership.new
     @invite = Invite.new
-    # Tasks this week
     @tasks_this_week = @group.tasks.where(due_date: Date.today..(Date.today + 7)).count
-    # Data for pie-chart
     @data_for_chart = [{name: "Weekly Tasks", y: @tasks_this_week} ]
-    # Tasks completed by each member for week
     @members.each do |member|
       @data_for_chart.push({
         name: member.name, 
